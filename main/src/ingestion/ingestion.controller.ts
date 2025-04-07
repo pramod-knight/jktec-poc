d16c2c6e13bac48bd7bca61225054d817cc8a7be
Pramod Kumar <pramod.kmr9166@gmail.com>
2025-04-07 11:22:28 +0530

    swagger: api documentation updated for ingestion

--- main/src/ingestion/ingestion.controller.ts
+++ main/src/ingestion/ingestion.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, HttpStatus } from '@nestjs/common';
 import { IngestionService } from './ingestion.service';
 import { CreateIngestionDto } from './dto/create-ingestion.dto';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { Roles } from '../auth/role.decorator';
 import { RoleCheckGuard } from '../auth/role.guard';
@@ -13,6 +13,18 @@ export class IngestionController {
   constructor(private readonly ingestionService: IngestionService) {}
 
   @ApiBearerAuth()
+  @ApiResponse({
+      status: HttpStatus.CREATED,
+      description: "Ingestion created",
+    })
+    @ApiResponse({
+      status: 500,
+      description: "Internal Server Error",
+    })
+    @ApiResponse({status:401,description:'Aunthorized to access'})
+    @ApiResponse({status:400,description:'Validation failed'})
+    @ApiResponse({status:403,description:'Resource access denied'})
+    @ApiResponse({status:429,description:'To many requests'})
   @UseGuards(JwtAuthGuard,RoleCheckGuard)
   @UseGuards(ThrottlerGuard)
   @Roles([RoleEnum.ADMIN,RoleEnum.EDITOR])
@@ -23,6 +35,18 @@ export class IngestionController {
 
   @Get(':id')
   @ApiBearerAuth()
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "Ingestion details",
+  })
+  @ApiResponse({
+    status: 500,
+    description: "Internal Server Error",
+  })
+  @ApiResponse({status:401,description:'Aunthorized to access'})
+  @ApiResponse({status:400,description:'Validation failed'})
+  @ApiResponse({status:403,description:'Resource access denied'})
+  @ApiResponse({status:429,description:'To many requests'})
   @UseGuards(JwtAuthGuard,RoleCheckGuard)
   @UseGuards(ThrottlerGuard)
   @Roles([RoleEnum.ADMIN,RoleEnum.EDITOR,RoleEnum.VISITOR])
